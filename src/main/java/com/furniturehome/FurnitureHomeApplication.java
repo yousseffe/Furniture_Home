c457ce81a542895ade53ed5bd78e46f3a5e64853
Duaa AbdelAti AbdelAzeem <164356690+DuaA-A@users.noreply.github.com>
2025-08-21 18:06:01 +0300

    setup the project

---
+++ src/main/java/com/furniturehome/FurnitureHomeApplication.java
@@ -0,0 +1,13 @@
+package com.furniturehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FurnitureHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FurnitureHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/furniturehome/FurnitureHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furniturehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurnitureHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
