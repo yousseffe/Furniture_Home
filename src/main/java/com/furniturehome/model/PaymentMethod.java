714cffab84d613bda589b381c5da82cb97f22801
Mariam Amro <mariam.seifeldin.2005@gmail.com>
2025-08-25 02:32:32 +0300

    PaymentDetails created

---
+++ src/main/java/com/furniturehome/model/PaymentMethod.java
@@ -0,0 +1,8 @@
+package com.furniturehome.model;
+
+public enum PaymentMethod {
+    CREDIT_CARD,
+    PAYPAL,
+    BANK_TRANSFER,
+    CASH_ON_DELIVERY
+}
---
+++ src/main/java/com/furniturehome/model/PaymentStatus.java
@@ -0,0 +1,8 @@
+package com.furniturehome.model;
+
+public enum PaymentStatus {
+    PENDING,
+    COMPLETED,
+    FAILED,
+    REFUNDED
+}
