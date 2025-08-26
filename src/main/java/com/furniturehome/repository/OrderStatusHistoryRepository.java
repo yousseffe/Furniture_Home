7d8d8f37ea2a4ee1e137a1973a515c2944a5e020
Mariam Amro <mariam.seifeldin.2005@gmail.com>
2025-08-26 07:16:02 +0300

    Order-centric repositories created

---
+++ src/main/java/com/furniturehome/repository/OrderStatusHistoryRepository.java
@@ -0,0 +1,11 @@
+package com.furniturehome.repository;
+
+import com.furniturehome.model.OrderStatusHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface OrderStatusHistoryRepository extends JpaRepository<OrderStatusHistory, UUID> {
+}
