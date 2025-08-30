ecc2976c79c41db7f1c4690e29ce7dae5f2b5f1f
nadaassad <136029252+nadaassad@users.noreply.github.com>
2025-08-30 22:07:24 +0300

    update security config

--- src/main/java/com/furniturehome/security/SecurityConfig.java
+++ src/main/java/com/furniturehome/security/SecurityConfig.java
@@ -17,6 +17,9 @@ public class SecurityConfig {
                 // Public routes
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/api/orders/**").permitAll()
+                .requestMatchers("api/payment/**").permitAll()
+                .requestMatchers("api/review/**").permitAll()
+                .requestMatchers("api/storesetting/**").permitAll()
 //              .requestMatchers("/api/users/**").permitAll()
 //              .requestMatchers("/api/cart/**").permitAll()
 
@@ -29,6 +32,23 @@ public class SecurityConfig {
                 // Categories (ADMIN only for all methods)
                 .requestMatchers("/api/categories/**").hasRole("ADMIN")
 
+                //payment
+                .requestMatchers(HttpMethod.GET,"api/payment/**").hasAnyRole("CUSTOMER", "ADMIN")
+                .requestMatchers(HttpMethod.POST,"api/payment/**").hasRole("CUSTOMER")
+                .requestMatchers(HttpMethod.PUT,"api/payment/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE,"api/payment/**").hasRole("CUSTOMER")
+
+                    //review
+                .requestMatchers(HttpMethod.GET,"api/review/**").hasAnyRole("CUSTOMER", "ADMIN")
+                            .requestMatchers(HttpMethod.POST,"api/review/**").hasRole("CUSTOMER")
+//                            .requestMatchers(HttpMethod.PUT,"api/review/**").hasRole("CUSTOMER")
+                            .requestMatchers(HttpMethod.DELETE,"api/review/delete/**").hasRole("CUSTOMER")
+
+                    //store setting
+                            .requestMatchers(HttpMethod.GET,"api/storesetting/**").hasAnyRole("CUSTOMER", "ADMIN")
+                            .requestMatchers(HttpMethod.PUT,"api/storesetting/**").hasRole("ADMIN")
+                            .requestMatchers(HttpMethod.DELETE,"api/storesetting/**").hasRole("ADMIN")
+                            .requestMatchers(HttpMethod.PUT,"api/storesetting/**").hasRole("ADMIN")
                 // Everything else requires authentication
                 .anyRequest().authenticated()
             );
